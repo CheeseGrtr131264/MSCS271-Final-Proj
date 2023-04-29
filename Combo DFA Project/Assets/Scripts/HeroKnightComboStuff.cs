93ec007409fde5f4312c6d0233ba191c995c686e
CheeseGrtr131264 <johnsend5268@my.uwstout.edu>
2023-04-29 15:22:58 -0500

    Update HeroKnightComboStuff.cs

--- Combo DFA Project/Assets/Scripts/HeroKnightComboStuff.cs
+++ Combo DFA Project/Assets/Scripts/HeroKnightComboStuff.cs
@@ -8,14 +8,16 @@ public class HeroKnightComboStuff : MonoBehaviour
     [SerializeField] float falloutTime;
     private Animator animator;
 
+    
     //Example
-    int[,] delta = new int[8, 2] { { 0, 1 }, { 2, 3 }, { 4, 5 }, { 6, 0 }, { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 7 } };
+    int[,] delta;
     // stored in the structure of [ [P] , [K] , [R] , [L] , [U] , [D] ]
     string[] combos = { "RPK", "LRLP", "UPKL", "PDLR", "DPU", "KLPDUR" };
     private string comboString; 
     private float falloutTimer;
     public string[,] sortedCombos;
     List<List<int>> pre_delta;
+    Queue<(int, string[])> nodesToImplement;
     
     int nodeNum = 0;
 
@@ -23,9 +25,11 @@ public class HeroKnightComboStuff : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        nodesToImplement = new Queue<(int, string[])>();
         pre_delta = new List<List<int>>();
+        InitializeDelta();
         //Acount for first Node
-        RecursiveDFAGen(combos, nodeNum );
+        DFAGen();
     }
 
     //Called by ShowCombo
@@ -62,6 +66,20 @@ public class HeroKnightComboStuff : MonoBehaviour
             }
         }
     }
+
+    void InitializeDelta()
+    {
+        int maxSize = 0;
+        for (int i = 0; i < combos.Length; i++)
+        {
+            for(int j = 0; j < combos[i].Length; j++)
+            {
+                maxSize++;
+            }
+        }
+        delta = new int[maxSize, 6];
+    }
+
     void AddToCombo(char letter)
     {
         //append letter to combo string
@@ -85,49 +103,70 @@ public class HeroKnightComboStuff : MonoBehaviour
         comboString = "";
     }
 
-    void RecursiveDFAGen(string[] states, int nodeIndex)
+    void DFAGen()
     {
+        int iterations = 0;
+        //First iteration
+
+        StateGen(0, combos);
+        while (nodesToImplement.Count != 0 && iterations < 1000)
+        {
+            iterations++;
+            (int, string[]) nextInfo = nodesToImplement.Dequeue();
+            StateGen(nextInfo.Item1, nextInfo.Item2);
+        }
+    }
+
+    void StateGen(int nodeIndex, string[] states)
+    {
+        int thisNodeIndex = nodeIndex;
+
+        //Next is where we store the node's future connections
+        //Will be passed into future iterations
         //                  p    k    r    l    u    d
         string[,] next = { { }, { }, { }, { }, { }, { } };
+        //Maximum number of potential states, probably less than this but needed for array generation
         int maxLength = states.Length;
+        //Initialized so each index could potentially hold all future connections
         next = new string[6,maxLength];
+        //Holds the number of connections for each action
         int[] lengths = new int[6];
 
+        //Sort into relevant group, Cut the first letter
         for (int i = 0; i < states.Length; i++)
         {
             int relevantArrayLength = -1;
+            //Make lower for less errors
             char firstChar = char.ToLower(states[i][0]);
+
+            //Sorting here
+            //For each, set next to be string - first character
+            //increase lengths by 1
             switch (firstChar)
             {
                 case 'p':
-                    relevantArrayLength = lengths[0];
-                    next[0, relevantArrayLength] = states[i];
-                    relevantArrayLength++;
+                    next[0, lengths[0]] = states[i].Substring(1);
+                    lengths[0]++;
                     break;
                 case 'k':
-                    relevantArrayLength = lengths[1];
-                    next[1, relevantArrayLength] = states[i].Substring(1);
-                    relevantArrayLength++;
+                    next[1, lengths[1]] = states[i].Substring(1);
+                    lengths[1]++;
                     break;
                 case 'r':
-                    relevantArrayLength = lengths[2];
-                    next[2, relevantArrayLength] = states[i].Substring(1);
-                    relevantArrayLength++;
+                    next[2, lengths[2]] = states[i].Substring(1);
+                    lengths[2]++;
                     break;
                 case 'l':
-                    relevantArrayLength = lengths[3];
-                    next[3, relevantArrayLength] = states[i].Substring(1);
-                    relevantArrayLength++;
+                    next[3, lengths[3]] = states[i].Substring(1);
+                    lengths[3]++;
                     break;
                 case 'u':
-                    relevantArrayLength = lengths[4];
-                    next[4, relevantArrayLength] = states[i].Substring(1);
-                    relevantArrayLength++;
+                    next[4, lengths[4]] = states[i].Substring(1);
+                    lengths[4]++;
                     break;
                 case 'd':
-                    relevantArrayLength = lengths[5];
-                    next[5, relevantArrayLength] = states[i].Substring(1);
-                    relevantArrayLength++;
+                    next[5, lengths[5]] = states[i].Substring(1);
+                    lengths[5]++;
                     break;
                 default:
                     Debug.LogError($"Character {firstChar} is not recognized");
@@ -135,6 +174,7 @@ public class HeroKnightComboStuff : MonoBehaviour
             }
         }
 
+        //Print for debug purposes
         for (int i = 0; i < states.Length; i++)
         {
             for(int j = 0; j <= lengths[i]; j++)
@@ -142,20 +182,43 @@ public class HeroKnightComboStuff : MonoBehaviour
                 
                 print(next[i, j]);
             }
-           
         }
+
+
+        //Initialize future connections for this node
         int[] specific_delta = {0, 0, 0, 0, 0, 0};
         for (int i = 0; i < states.Length; i++)
         {
+            //If there will be a future node, 
+
+            //ERROR HERE. PROBS NEEDS TO BE 6 OR SOMETHIN
             if (next.GetLength(i) != 0)
             {
                 nodeNum++;
                 specific_delta[i] = nodeNum;
+                int length = next.GetLength(i);
+                string[] nextArray = new string[length];
+                for (int j = 0; j < length; j++)
+                {
+                    nextArray[j] = next[i, j];
+                }
 
+                (int, string[]) node = (nodeNum, nextArray);
+                nodesToImplement.Enqueue(node);
+            }
+            //End of the combo
+            else
+            {
+                specific_delta[i] = -1;
             }
 
         }
+
+        //Set delta to proper values
+        for(int i = 0; i < 6; i++)
+        {
+            delta[thisNodeIndex, i] = specific_delta[i];
+        }
        
-        sortedCombos = next;
     }
 }
